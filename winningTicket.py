f7180c41369178f8d00fc3a31a9036fc7da04523
nahidalam <shapla@gmail.com>
2018-01-10 19:10:44 -0800

    added comments and more return condition from dfs

--- winningTicket.py
+++ winningTicket.py
@@ -2,15 +2,17 @@
 Runs on Python 3.x
 Author: Nahid Alam
 '''
+
+totalExpectedNum = 7
+
 def findTicket(numStr):
     numListList = [list(map(int, x)) for x in numStr]
     for l in numListList:
         length = len(l)
         if length <7 or length > 14:
             continue
-        res = []
+        res = []  #result list containing the result
         dfs(l, res, 0)
-        #print(res)
         if res:
             #print result in the desired format
             str1 = ''.join(list(map(str, l)))
@@ -21,14 +23,17 @@ def findTicket(numStr):
 
 def dfs(numList, res, index):
     if index == len (numList):
-
-        if len(res) == 7:
+        if len(res) == totalExpectedNum:
             return res
         else:
             return  []  #return an empty list
+    if index < len(numList) and len(res) == totalExpectedNum:
+        #we already have 7 items in the result list but the string contains more number
+        #failed case so return an empty list
+        return []
 
 
-    num1 = numList[index]
+    num1 = numList[index] #num1 is the 1 digit number
     if num1 >=1 and num1 <=59 and (num1 not in res):
         res.append(num1)
         result = dfs(numList, res, index+1)
@@ -36,7 +41,7 @@ def dfs(numList, res, index):
             return result
         res.remove(num1)
     if index < len(numList) - 1:
-        num = numList[index] * 10 + numList[index+1]
+        num = numList[index] * 10 + numList[index+1] #num is a 2 digit number
         if num >=1 and num <=59 and (num not in res):
             res.append(num)
             result = dfs(numList, res, index+2)
